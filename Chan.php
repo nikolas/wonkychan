e0f11efad604a27080273e9e1f62eed29cf49ea8
Nik Nyby <niknyby@gmail.com>
2011-06-06 23:43:14 -0700

    okay now it works with php strict errors

--- Chan.php
+++ Chan.php
@@ -13,48 +13,6 @@ class Chan {
 		}
 	}
 
-	public function out() {
-?>
-<!doctype html>
-<html>
-<head>
-<meta name="generator" content="WonkyChan 0.1">
-<title>wonkychan</title>
-<?php echo $this->style(); ?>
-</head>
-<body>
-	<div id="wonky-contained">
-		<?php echo $this->header(); ?>
-		<p>
-			<h1><a href="<?php echo $this->site_url; ?>">Wonkychan</a></h1>
-			<?php echo $this->form(); ?>
-			<?php var_dump(iterator_to_array($this->showDorps())); ?>
-		</p>
-	</div>
-</body>
-</html>
-<?php
-	}
-
-	private function header() {
-?>
-<div class="header">
-<?php print_r($this->get); ?>
-<?php print_r($this->post); ?>
-</div>
-<?php
-	}
-
-	private function form() {
-?>
-<form name="dorp" enctype="multipart/form-data" method="post" action="index.php">
-<textarea id="words" name="words"></textarea>
-<input type="file" name="picture" id="picture" />
-<input type="submit" />
-</form>
-<?
-	}
-
 	private function style() {
 ?>
 <link rel="stylesheet" href="blueprint/screen.css" type="text/css" media="screen, projection">
@@ -81,8 +39,50 @@ class Chan {
 <?php
 	}
 
+	private function header() {
+?>
+<div class="header">
+<?php print_r($this->get); ?>
+<?php print_r($this->post); ?>
+</div>
+<?php
+	}
+
 	private function showDorps() {
 		return $this->db->dorps->find(array('words'));
+	}
+
+	private function form() {
+?>
+<form name="dorp" enctype="multipart/form-data" method="post" action="index.php">
+<textarea id="words" name="words"></textarea>
+<input type="file" name="picture" id="picture" />
+<input type="submit" />
+</form>
+<?php
+	}
+
+	public function out() {
+?>
+<!doctype html>
+<html>
+<head>
+<meta name="generator" content="WonkyChan 0.1">
+<title>wonkychan</title>
+<?php echo $this->style(); ?>
+</head>
+<body>
+	<div id="wonky-contained">
+		<?php echo $this->header(); ?>
+		<p>
+			<h1><a href="<?php echo $this->site_url; ?>">Wonkychan</a></h1>
+			<?php echo $this->form(); ?>
+			<?php var_dump(iterator_to_array($this->showDorps())); ?>
+		</p>
+	</div>
+</body>
+</html>
+<?php
 	}
 }
 ?>

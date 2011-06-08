d7456790951c2b590ea80144f9dbad0c957ea333
Nik Nyby <niknyby@gmail.com>
2011-06-07 22:41:15 -0700

    added config examples

--- config.example.php
+++ config.example.php
@@ -1,7 +1,16 @@
 <?php
+// e.g.: wonky_user
 define('DB_USER', '');
+
+// e.g.: wonky_pass
 define('DB_PASS', '');
+
+// e.g.: wonky_db
 define('DB_NAME', '');
+
+// e.g.: /wonkychan/
 define('SITE_PATH', '');
+
+// e.g.: /var/www/localhost/htdocs/wonkychan/
 define('SITE_FS_PATH', '');
 ?>

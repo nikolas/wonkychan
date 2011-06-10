a9a6d94fa77f7dc30aa13ba7af790dd119cf0f48
Nik Nyby <niknyby@gmail.com>
2011-06-10 03:21:14 -0700

    added start to wonkyadmin

--- Chan.php
+++ Chan.php
@@ -4,6 +4,7 @@ class Chan {
 	private $site_path, $site_fs_path;
 	private $alert;
 	private $route;
+	private $adminName;
 
 	public function __construct(
 			$db,
@@ -21,22 +22,19 @@ class Chan {
 		$this->site_fs_path = $site_fs_path;
 		$this->alert = '';
 
+		if (isset($_SESSION) && array_key_exists('admin_name', $_SESSION)) {
+			$this->adminname = $_SESSION['admin_name'];
+		} else {
+			$this->adminName = null;
+		}
+
 		$route = preg_replace("|{$this->site_path}|", '', $_SERVER['REQUEST_URI']);
 		$route = preg_replace('/\/\//', '/', $route);
 		$route = preg_replace('/^\//', '', $route);
 		$this->route = preg_split('/\//', $route);
 
-		if (!empty($this->files) && !empty($this->files['picture']['name'])) {
-			$new_filename = $this->site_fs_path . '/d/' . basename($files['picture']['name']);
-			if (move_uploaded_file($this->files['picture']['tmp_name'], $new_filename)) {
-				$this->alert .= ':) ';
-				$this->alert .= $this->db->dorps
-						->insert(array('picture' => basename($files['picture']['name'])));
-			} else {
-				$this->alert .= 'woops';
-			}
-		} else {
-			$this->alert .= '>:(';
+		if (!empty($this->post) && array_key_exists('redir', $this->post)) {
+			$this->route[0] = $this->post['redir'];
 		}
 	}
 
@@ -64,12 +62,20 @@ class Chan {
 		width: 960px;
 		margin: 0px auto 0px auto;
 	}
-	.header, .dorps {
+	.header {
 		background-color: #9ff904;
 		color: black;
 		padding: 1em;
 	}
 </style>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+	$('.dorps').delegate('img.pic', 'hover', function(event) {
+		//alert(this);
+	});
+})
+</script>
 <?php
 	}
 
@@ -82,15 +88,33 @@ class Chan {
 	}
 
 	private function showDorps() {
+		if (!empty($this->files) && !empty($this->files['picture']['name'])) {
+			$new_filename = $this->site_fs_path . '/d/' . basename($files['picture']['name']);
+			if (move_uploaded_file($this->files['picture']['tmp_name'], $new_filename)) {
+				$this->alert .= ':) ';
+				$this->alert .= $this->db->dorps
+						->insert(array('picture' => basename($files['picture']['name'])));
+			} else {
+				$this->alert .= 'woops';
+			}
+		} else {
+			$this->alert .= '>:(';
+		}
 		$cursor = $this->db->dorps
 				->find()->sort(array('$natural' => -1));
 		$cursor->rewind();
-		$s = '';
+		$s = '<div class="dorps">';
 		foreach ($cursor as $d) {
 			if (array_key_exists('picture', $d)) {
 				$s .= "<img class=\"pic\" src=\"{$this->site_path}/d/{$d['picture']}\" />";
 			}
 		}
+		$s .= '</div>';
+?>
+<script>
+$('.header').html('<?php echo $this->alert; ?>')
+</script>
+<?php
 		return $s;
 	}
 
@@ -100,7 +124,7 @@ class Chan {
 		$cursor->rewind();
 		$s = '';
 		foreach ($cursor as $d) {
-			$s .= "yeah this is {$d['title']} uhhhhh";
+			$s .= "{$d['title']}";
 		}
 
 		$s .= '<table>'
@@ -122,6 +146,12 @@ class Chan {
 
 		$s .= '</table>';
 
+?>
+<script>
+var $title = $('title');
+$title.text( $title.text() + ' forum' );
+</script>
+<?php
 		return $s;
 	}
 
@@ -148,7 +178,99 @@ class Chan {
 		return $s;
 	}
 
-	private function form() {
+	private function showAdmin() {
+		if (!empty($this->post) && array_key_exists('formname', $this->post)) {
+			$this->alert = '';
+			switch ($this->post['formname']) {
+				case 'admin_create':
+					if (strcmp($this->post['admin_name'], '') == 0) {
+						$this->alert .= 'put in a name';
+						break;
+					}
+					if (strcmp($this->post['admin_pass'], '') == 0) {
+						$this->alert .= 'put in a password';
+						break;
+					}
+					if (strcmp($this->post['admin_pass'], $this->post['admin_pass_confirm']) != 0) {
+						$this->alert .= 'passwords dont match';
+						break;
+					}
+					$hashed = crypt($this->post['admin_pass']);
+					$r = $this->db->admins
+							->insert(array('username' => $this->post['admin_name'], 'password' => $hashed));
+					if ($r == 1) {
+						print 'user made :)';
+						session_start();
+						print_r($_SESSION);
+						$_SESSION['admin_name'] = $this->post['admin_name'];
+						print_r($_SESSION);
+					}
+					break;
+				case 'admin_login':
+					break;
+				case 'admin':
+					break;
+				default:
+					break;
+			}
+		}
+
+		if ($this->adminName) {
+?>
+<p>Welcome, <?php echo $this->adminName; ?> :)</p>
+<form class="admin" name="admin" method="post" action="<?php echo $this->site_path . '/a/'; ?>">
+<input type="hidden" name="redir" value="a" />
+<input type="hidden" name="formname" value="admin" />
+<input type="submit" />
+</form>
+<?php
+		} else {
+			if ($this->db->admins->find()->count() < 1) {
+			print_r($this->post);
+?>
+<p>There's no admin user. Make one :)</p>
+<form class="admin_create" name="admin_create" method="post" action="<?php echo $this->site_path . '/a/'; ?>">
+<label for="admin_name">Username</label>
+<input type="text" name="admin_name" id="admin_name" />
+<br />
+<label for="admin_pass">Password</label>
+<input type="password" name="admin_pass" id="admin_pass" />
+<br />
+<label for="admin_pass_confirm">Password again</label>
+<input type="password" name="admin_pass_confirm" id="admin_pass_confirm" />
+<input type="hidden" name="redir" value="a" />
+<input type="hidden" name="formname" value="admin_create" />
+<br />
+<input type="submit" />
+</form>
+<?php
+			} else {
+?>
+<p>Log in to wonkyadmin</p>
+<form class="admin_login" name="admin_login" method="post" action="<?php echo $this->site_path . '/a/'; ?>">
+<label for="admin_name">Username</label>
+<input type="text" name="admin_name" id="admin_name" />
+<br />
+<label for="admin_pass">Password</label>
+<input type="password" name="admin_pass" id="admin_pass" />
+<br />
+<input type="hidden" name="redir" value="a" />
+<input type="hidden" name="formname" value="admin_login" />
+<input type="submit" />
+</form>
+<?php
+			}
+		}
+?>
+<script>
+var $title = $('title');
+$title.text( $title.text() + ' admin' );
+$('.header').html('<?php echo $this->alert; ?>')
+</script>
+<?php
+	}
+
+	private function dorpForm() {
 ?>
 <form class="dorp" name="dorp" enctype="multipart/form-data" method="post" action="<?php echo $this->site_path . '/index.php'; ?>">
 <input type="file" name="picture" id="picture" />
@@ -160,14 +282,17 @@ class Chan {
 
 	private function yield() {
 		switch ($this->route[0]) {
-			case 'f':
+			case 'a': // admin
+				return $this->showAdmin();
+				break;
+			case 'f': // forums
 				if (!isset($this->route[1]) || !$this->route[1]) {
 					continue;
 				}
 				return $this->showForum($this->route[1]);
 				break;
 			default:
-				return $this->showForums() . $this->form() . $this->showDorps();
+				return $this->showForums() . $this->dorpForm() . $this->showDorps();
 		}
 	}
 

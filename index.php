e2e01e12453708ef9cbc435a1b76f14ef5e83b54
Nik Nyby <nik@alpha.home>
2018-02-25 23:29:04 -0500

    update mongo library

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+require 'vendor/autoload.php';
+
 require('config.php');
 require('Chan.php');
 
-$mongo = new Mongo('mongodb://'.DB_USER.':'.DB_PASS.'@localhost/'.DB_NAME);
-$db = $mongo->selectDB(DB_NAME);
+$mongo = new MongoDB\Client('mongodb://'.DB_USER.':'.DB_PASS.'@localhost/'.DB_NAME);
+
+// TODO: use DB_NAME
+$db = $mongo->wonkychan;
 
 $chan = new Chan($db, SITE_PATH, SITE_FS_PATH, $_GET, $_POST, $_FILES);
 
